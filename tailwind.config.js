933ce0a66d38fdd5fdde6d9f2b28d27a42ee62ae
LucasRomero <romerolucasdaniel@outlook.com>
2023-06-27 22:27:43 +1200

    fix styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        'LightCyan': 'hsl(193, 38%, 86%)',
-        'NeonGreen': 'hsl(150, 100%, 66%)',
-        'GrayishBlue': 'hsl(217, 19%, 38%)',
-        'DarkGrayishBlue': 'hsl(217, 19%, 24%)',
-        'DarkBlue': 'hsl(218, 23%, 16%)',
+      colors: {
+        LightCyan: "hsl(193, 38%, 86%)",
+        NeonGreen: "hsl(150, 100%, 66%)",
+        GrayishBlue: "hsl(217, 19%, 38%)",
+        DarkGrayishBlue: "hsl(217, 19%, 24%)",
+        DarkBlue: "hsl(218, 23%, 16%)",
       },
-      fontFamily:{
-        Manrope: ['Manrope', 'sans-serif'] 
-      }
+      fontFamily: {
+        Manrope: ["Manrope", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
